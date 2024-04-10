a54135ede60f48bb6ac048fe0d57531f0211fc11
Pierre Huguet <pierre@delange.design>
2024-04-10 01:19:12 -0700

    Add project files.

---
+++ MAuiAutoGridTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAuiAutoGridTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAuiAutoGridTest/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MAuiAutoGridTest
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+
+}
